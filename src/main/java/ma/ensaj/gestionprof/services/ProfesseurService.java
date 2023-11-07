18df74d45fd897f784e0b2bfbbf3ee0b8812a1ec
CamaraCamara123 <aboudramanecamara9@gmail.com>
2023-11-07 15:48:34 +0100

    commit service

---
+++ src/main/java/ma/ensaj/gestionprof/services/ProfesseurService.java
@@ -0,0 +1,47 @@
+package ma.ensaj.gestionprof.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ma.ensaj.gestionprof.dao.IDao;
+import ma.ensaj.gestionprof.entities.Professseur;
+import ma.ensaj.gestionprof.repository.ProfesseurRepository;
+
+@Service
+public class ProfesseurService implements IDao<Professseur>{
+
+    @Autowired
+    ProfesseurRepository professeurRepository;
+    @Override
+    public Professseur create(Professseur o) {
+       return professeurRepository.save(o);
+    }
+
+    @Override
+    public List<Professseur> findAll() {
+        return professeurRepository.findAll();
+    }
+
+    @Override
+    public Professseur update(Professseur o) {
+        return professeurRepository.save(o);
+    }
+
+    @Override
+    public boolean delete(Professseur o) {
+        try {
+            professeurRepository.delete(o);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Professseur findById(Long id) {
+        return professeurRepository.findById(id).orElse(null);
+    }
+    
+}
---
+++ src/main/java/ma/ensaj/gestionprof/services/SpecialiteService.java
@@ -0,0 +1,45 @@
+package ma.ensaj.gestionprof.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ma.ensaj.gestionprof.dao.IDao;
+import ma.ensaj.gestionprof.entities.Specialite;
+import ma.ensaj.gestionprof.entities.Specialite;
+import ma.ensaj.gestionprof.repository.SpecialiteRepository;
+
+public class SpecialiteService implements IDao<Specialite> {
+    @Autowired
+    SpecialiteRepository specialiteRepository;
+
+    @Override
+    public Specialite create(Specialite o) {
+        return specialiteRepository.save(o);
+    }
+
+    @Override
+    public List<Specialite> findAll() {
+        return specialiteRepository.findAll();
+    }
+
+    @Override
+    public Specialite update(Specialite o) {
+        return specialiteRepository.save(o);
+    }
+
+    @Override
+    public boolean delete(Specialite o) {
+        try {
+            specialiteRepository.delete(o);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Specialite findById(Long id) {
+        return specialiteRepository.findById(id).orElse(null);
+    }
+}
