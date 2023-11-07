7186c8056239d131705712029363620794ddaca1
ghita-baghdad <ghitabaghdad12@gmail.com>
2023-11-07 15:24:20 +0100

    professeur

--- src/main/java/ma/ensaj/gestionprof/entities/Professseur.java
+++ src/main/java/ma/ensaj/gestionprof/entities/Professseur.java
@@ -1,5 +1,31 @@
 package ma.ensaj.gestionprof.entities;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Data
 public class Professseur {
-    
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	private String prenom;
+	private String telephone;
+	private String email;
+	@Temporal(TemporalType.DATE)
+	private Date dateEmbauche;
+
+	@ManyToOne
+	private Specialite specialite;
+
 }
