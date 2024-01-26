ca0dce3c5fa12a6381ad71d3f1a8908896591108
BrightProgrammer7 <e-akhmim.a@ucd.ma>
2024-01-26 18:49:31 +0000

    gestionprof

--- src/main/java/ma/ensaj/gestionprof/GestionprofApplication.java
+++ src/main/java/ma/ensaj/gestionprof/GestionprofApplication.java
@@ -3,6 +3,7 @@ package ma.ensaj.gestionprof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GestionprofApplication {
 
